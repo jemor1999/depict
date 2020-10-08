6dfb0502460b951faa17be8912b37d2b756decb0
jemor <1625005676@qq.com>
2020-10-09 07:24:40 +0800

    初始化配置

---
+++ main/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by jemor on 2020/10/8.
+//
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char *argv[]){
+    QApplication a(argc, argv);
+
+    QMainWindow qMainWindow;
+    qMainWindow.show();
+
+    return a.exec();
+}
